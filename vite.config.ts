5d3917e72fa820c0d32f541b46b6d74f151172c5
Nguyen Duy Manh <manh.nd@elines.vn>
2026-08-03 22:57:52 +0700

    Add wedding link preview image

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { readFileSync } from 'node:fs'
+import { fileURLToPath } from 'node:url'
+
+const socialImage = fileURLToPath(
+  new URL('./src/img/2aOboQnzQtmOrfUFCAXHhKZPEpnmrLBmLIS0MF3w.jpg', import.meta.url),
+)
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'emit-social-image',
+      generateBundle() {
+        this.emitFile({
+          type: 'asset',
+          fileName: 'og-image.jpg',
+          source: readFileSync(socialImage),
+        })
+      },
+    },
+  ],
   base: '/',
 })
